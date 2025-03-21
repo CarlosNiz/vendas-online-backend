928e1e84c164f8f855b99e84912a460d6d9fdbfc
Carlos Niz <c4rlos.niz@gmail.com>
2025-03-21 10:49:53 -0300

    feat: insert validation in address

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserEntity } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class UserService {
@@ -25,4 +26,18 @@ export class UserService {
     async getAllUser(): Promise<UserEntity[]> {
         return this.userRepository.find();
     };
+
+    async findUserById(userId: number): Promise<UserEntity> {
+        const user = await this.userRepository.findOne({
+            where: {
+                id: userId
+            }
+        });
+
+        if (!user) {
+            throw new NotFoundException(`UserId: ${userId} Not Found`);
+        }
+
+        return user;
+    }
 }
